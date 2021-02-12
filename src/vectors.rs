0c40588fa7357d8c36919001fa05de1e904c602f
dankeyy <dankeyy@protonmail.com>
2021-02-12 19:39:45 +0200

    some more functionality to vec3

--- src/vectors.rs
+++ src/vectors.rs
@@ -43,8 +43,80 @@ impl Vector3 {
         dot(&self, &self).sqrt()
     }
 
-    
 
+    pub fn normalize(&self) -> Vector3 {
+        let n: f64 = self.magnitude();
 
+        Vector3 { 
+            x: self.x / n,
+            y: self.y / n,
+            z: self.z / n,
+        }
+    }
 
 }
+impl Add for Vector3 {
+    type Output = Vector3;
+
+    fn add(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+
+
+impl Sub for Vector3 {
+    type Output = Vector3;
+
+    fn sub(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+
+impl Mul for Vector3 {
+    type Output = Vector3;
+
+    fn mul(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
+    }
+}
+
+
+impl Div for Vector3 {
+    type Output = Vector3;
+
+    fn div(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        }
+    }
+}
+
+
+impl Neg for Vector3 {
+    type Output = Vector3;
+
+    fn neg(&self, other: &Vector3) -> Vector3 {
+        Vector3 {
+            x: -self.x
+            y: -self.y
+            z: -self.z
+        }
+    }
+}
+
